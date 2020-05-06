425d2edb9a5922350dc897bce9e3b30a54b84812
Jeff Peters <petersjeff288@gmail.com>
2020-05-05 22:42:36 -0400

    small update to movieGenRepo

--- src/main/java/com/bmdb/db/MovieGenreRepository.java
+++ src/main/java/com/bmdb/db/MovieGenreRepository.java
@@ -9,6 +9,6 @@ import com.bmdb.business.MovieGenre;
 public interface MovieGenreRepository extends JpaRepository<MovieGenre, Integer> {
 	List<MovieGenre> findAllByMovieId(int movieId);
 	List<MovieGenre> findAllByGenreId(int genreId);
-		
+		//List<MovieGenre> movieGenres = movieGenreRepo.findAllByGenreId(genreId);
 	
 }
